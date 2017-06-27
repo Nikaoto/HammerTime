37aea3ecca418c8ed9c378295d00991f861af2ad
Nika Otiashvili <nikaoto@gmail.com>
2017-06-27 17:20:52 +0400

    fix clouds with scaling screens

--- cloudmanager.lua
+++ cloudmanager.lua
@@ -1,6 +1,7 @@
 cloudImageCount = 6
 cloudImages = {}
-local x1, x2, x3 = 0, display.width - 200, 300
+local x1, x2, x3 = 0, display.width - 200, display.width/10
+
 function initClouds()
   for i = 1, cloudImageCount do
     cloudImages[i] = love.graphics.newImage("/res/cloud"..i..".png")
@@ -8,13 +9,13 @@ function initClouds()
 end
 
 function drawClouds()
-  love.graphics.draw(cloudImages[1], x1, 1)
-  love.graphics.draw(cloudImages[2], x2, 400)
-  love.graphics.draw(cloudImages[3], x3, display.height - 400)
+  love.graphics.draw(cloudImages[1], x1, display.width / 100)
+  love.graphics.draw(cloudImages[2], x2, display.height / 2)
+  love.graphics.draw(cloudImages[3], x3, display.height / 2.2)
 end
 
 function updateClouds(dt)
-  x1 = x1 + dt * 10
-  x2 = x2 - dt * 20
-  x3 = x3 + dt * 17
+  x1 = x1 + dt * 9
+  x2 = x2 - dt * 23
+  x3 = x3 + dt * 15
 end
--- welcomescreen.lua
+++ welcomescreen.lua
@@ -2,8 +2,8 @@ require 'cloudmanager'
 local TAU = math.pi * 2
 local screenWidth, screenHeight = display.width, display.height
 --local screenWidth, screenHeight = love.graphics:getDimensions()
-local p, pe
-
+local canvas
+local particleSystem
 local MS = 1
 local PSIZE = 40
 
@@ -14,55 +14,39 @@ function loadWelcomeScreen()
 
   --local p = love.graphics.newImage "particle.png"
 
-  p = love.graphics.newCanvas(PSIZE, PSIZE)
+  canvas = love.graphics.newCanvas(PSIZE, PSIZE)
 
-  p:renderTo(function()
+  canvas:renderTo(function()
     --love.graphics.clear(255,255,255,0)
     --love.graphics.rectangle('fill', 0, 0, PSIZE, PSIZE)
     --love.graphics.circle('fill', PSIZE/2, PSIZE/2, PSIZE/2-2, 100)
     love.graphics.polygon('fill', 1, 16, 30, 16, 16, 1)
   end)
 
-  --p = love.graphics.newImage "particle.png"
-
-  --love.graphics.setBackgroundColor(9, 25, 27)
-
-  pe = {}
-  for i=1,2 do
-    pe[i] = love.graphics.newParticleSystem(p, 4096)
-    pe[i]:setParticleLifetime(0.1, 1.9)
-    pe[i]:setEmissionRate(850)
-    pe[i]:setSizes(1.5, 0.6)
-    --pe:setSizeVariation(1)
-    pe[i]:setLinearAcceleration(-10, -10, 10, 10) -- Random movement in all directions.
-    pe[i]:setLinearDamping(3)
-    --pe:setColors(255, 255, 255, 255, 255, 255, 255, 0) -- Fade to transparency.
-    pe[i]:setAreaSpread('uniform', screenWidth / 2, 0)
-    --pe:setDirection(3/4 * TAU)
-    --pe[i]:setRotation(0, TAU/4)
-    pe[i]:setDirection(math.pi * 3 / 2)
-  end
-  pe[1]:setPosition(screenWidth/2, screenHeight)
-  pe[2]:setPosition(screenWidth/2, screenHeight)
-
-  pe[1]:setSpeed(700, 410)
-  --pe[2]:setSpeed(-150, 0)
-
-	pe[1]:setColors(255, 190, 130, 200,   255, 80, 0, 130,  0, 0, 0, 0)
-	pe[2]:setColors(150, 255, 255, 200,   0, 255, 0, 130,   0, 0, 0, 0)
-
+  --canvas = love.graphics.newImage "particle.png"
+  particleSystem = love.graphics.newParticleSystem(canvas, 4096)
+  particleSystem:setParticleLifetime(0.1, 1.9)
+  particleSystem:setEmissionRate(850)
+  particleSystem:setSizes(1.5, 0.6)
+  particleSystem:setLinearAcceleration(-10, -10, 10, 10)
+  particleSystem:setLinearDamping(3)
+  particleSystem:setAreaSpread('uniform', screenWidth / 2, 0)
+  particleSystem:setDirection(math.pi * 3 / 2)
+  particleSystem:setPosition(screenWidth/2, screenHeight + 5)
+  particleSystem:setSpeed(700, 410)
+	particleSystem:setColors(255, 190, 130, 200,   255, 80, 0, 130,  0, 0, 0, 0)
   canvas = love.graphics.newCanvas(screenWidth * MS, screenHeight * MS)
 end
 
-
 function updateWelcomeScreen(dt)
   updateClouds(dt)
-  for i=1,2 do pe[i]:update(dt) end
+  particleSystem:update(dt)
 end
 
 function drawWelcomeScreen()
   drawSky(skyBG2)
   drawClouds()
+
   canvas:renderTo(function()
     --love.graphics.clear(0,0,0,0.01)
     love.graphics.setBlendMode('subtract', 'premultiplied')
@@ -70,19 +54,14 @@ function drawWelcomeScreen()
     love.graphics.rectangle('fill', 0, 0, screenWidth * MS, screenHeight * MS)
 
     love.graphics.setColor(255,255,255,255)
-    --love.graphics.setColor(255,0,255,255)
-    --love.graphics.setColor(HSL(h, 200, 200, 255))
     love.graphics.setBlendMode('alpha', 'premultiplied')
-    --for i=1,2 do love.graphics.draw(pe[i]) end
-    love.graphics.draw(pe[1])
+    love.graphics.draw(particleSystem)
   end)
 
   love.graphics.setBlendMode('alpha', 'alphamultiply')
   love.graphics.setColor(255,255,255,255)
-  --love.graphics.draw(pe)
   love.graphics.draw(canvas, 0, 0, 0, 1/MS, 1/MS)
 
 --draw PRESS ANY KEY TO START GAME
-  love.graphics.draw(titleImage,
-    (display.width - titleImage:getWidth()) / 2, 100)
+  love.graphics.draw(titleImage, (display.width - titleImage:getWidth()) / 2, display.height/10)
 end
